28fab9a189e304844160266b711f5c410ac0a272
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-14 02:41:53 -0400

    top half of page set up. Using slider to display options of AI engine

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
-
-
+import { Paper, Box, Card } from '@material-ui/core';
 import style from '../styles/Home.module.scss';
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {DetailsCard} from '../src/components/DetailsCard/DetailsCard';
+import data from '../src/text';
 
 export default function Home() {
+  
   return (
     <div className={style.home}>
-      <p> Hello world </p>
+      <div className={style.home__header}>
+        <h1 className={style.home__header_title}> Fun with GPT-3 </h1>
+        <p className={style.home__header_subhead}>
+          Shopify Developer Intern Challenge
+        </p>
+      </div>
+      <Paper elevation={2} variant="elevated" className={style.home__content}>
+        <div className={style.home__content_head}>
+          <h2 className={style.home__content_title}>Select an AI engine</h2>
+          <div className={style.home__content_options}>
+            <Swiper
+              className={style.home__swiper}
+            >
+              {data.options.map((opt, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    <DetailsCard
+                      title={opt.title}
+                      description={opt.description}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+        </div>
+      </Paper>
     </div>
-  )
+  );
 }
