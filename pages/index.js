9acfc73da17e72c2afc311e51e5bb1eb0fa51215
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-17 01:04:48 -0400

    response styling all set up for mobile to mid tablet size, keeping it at that size.

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,61 @@ import { PromptForm } from "../src/components/PromptForm/PromptForm";
 import PromptList from "../src/features/prompts/PromptList";
 import { HeroSection } from "../src/components/HeroSection/HeroSection";
 import { SwiperList } from "../src/components/SwiperList/SwiperList";
+import { WelcomeOptions } from "../src/components/WelcomeOptions/WelcomeOptions";
+import TravelBuddy from "../src/components/TravelBuddy/TravelBuddy";
 import axios from "axios";
+
 
 export default function Home() {
   
+  
+  const [travel, setTravel] = useState(false);
+  const [defaul, setDefaul] = useState(true);
   const [option, setOption] = useState("Davinci");
+
 
   const dispatch = useDispatch();
 
   const userOption = (opt) => {
     setOption(opt);
   };
+
+  const renderTravel = () => {
+    setTravel(true)
+    setDefaul(false);
+  }
+
+  const renderDefault = () => {  
+    setDefaul(true)
+    setTravel(false)
+  }
+
+  const display = () => {
+    
+    if (travel) {
+      return (
+        <TravelBuddy />
+      )
+    }
+    else {
+      null
+    }
+
+    if (defaul) {
+      return (
+        <div>
+          <SwiperList handler={userOption} />
+          <div className={style.home__form}>
+            <PromptForm handler={onSubmit} />
+          </div>
+          <div className={style.home__response}>
+            <PromptList />
+          </div>
+        </div>
+      );
+    }
+
+  }
 
   const onSubmit = async (values) => {
     const resp = await axios
@@ -46,14 +90,11 @@ export default function Home() {
     <div className={style.home}>
       <HeroSection />
       <Paper elevation={2} variant="elevation" className={style.home__content}>
-        <h1 className={style.home__title}>Select an AI engine </h1>
-        <SwiperList handler={userOption} />
-        <div className={style.home__form}>
-          <PromptForm handler={onSubmit} />
-        </div>
-        <div className={style.home__response}>
-          <PromptList />
-        </div>
+        <WelcomeOptions 
+          travelHandle = {renderTravel} 
+          defaultHandle = {renderDefault}
+          />
+        {display()}
       </Paper>
     </div>
   );
