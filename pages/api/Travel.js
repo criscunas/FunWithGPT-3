ab76bffd987f14b4d004546ca449610d809d8968
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-18 01:52:17 -0400

    removed api key

--- pages/api/Travel.js
+++ pages/api/Travel.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "",
 });
 const openai = new OpenAIApi(configuration);
 
