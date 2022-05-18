7e90183d33c25fa6097498205314c9b66d232278
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-18 02:00:20 -0400

    new api key env

--- pages/api/Travel.js
+++ pages/api/Travel.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: "",
+  apiKey: process.env.apiKey,
 });
 const openai = new OpenAIApi(configuration);
 
