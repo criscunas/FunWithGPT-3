72663877eb2760c92a80921b712e85199bcd7b82
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-21 01:03:47 -0400

    padding fix

--- src/components/TravelBuddy/TravelBuddy.js
+++ src/components/TravelBuddy/TravelBuddy.js
@@ -73,7 +73,10 @@ export default function TravelBuddy () {
         <TravelForm handler={onSubmit} />
 
         <div className={style.travel__form_random}>
-          <h2 className={style.travel__form_text}>
+          <h2
+            className={style.travel__form_text}
+            onClick={() => randomDestination()}
+          >
             {" "}
             Click here for a randomly generated destination{" "}
           </h2>
